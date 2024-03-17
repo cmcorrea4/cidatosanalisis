726818436c6f0af8bcecfbf8cb29c1036070b4e4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-17 16:37:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import streamlit as st
+from PIL import Image
+
 
 st.title('Análisis de datos de Sensores en Mi Ciudad')
+image = Image.open('grafana2.jpg')
+st.image(image)
+
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
    df1=pd.read_csv(uploaded_file)
