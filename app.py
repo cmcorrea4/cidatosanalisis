66c0a00dd06506f4a19f0c2b6405503656f65967
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-17 16:25:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ if uploaded_file is not None:
    df1=pd.read_csv(uploaded_file)
    st.write(df1)
    #Asegúrate de que la columna de fechas es de tipo datetime
-   df1['Fecha Inicio'] = pd.to_datetime(df1['Fecha Inicio'])
+   #df1['Fecha Inicio'] = pd.to_datetime(df1['Fecha Inicio'])
 
    # Define tu rango de tiempo
-   inicio = pd.to_datetime('2023-05-05')
-   fin = pd.to_datetime('2023-05-29')
-   st.write(inicio)
-   st.write(fin)
+   #inicio = pd.to_datetime('2023-05-05')
+   #fin = pd.to_datetime('2023-05-29')
+   #st.write(inicio)
+   #st.write(fin)
 
    # Filtra el DataFrame
-   df1_filtrado = df1[df1['Fecha Inicio'].between(inicio, fin)]
+   #df1_filtrado = df1[df1['Fecha Inicio'].between(inicio, fin)]
 
    # Muestra el DataFrame filtrado
-   st.write(df1_filtrado)
+   #st.write(df1_filtrado)
 else:
- st.warning('you need to upload a csv or excel file.')
+ st.warning('Necesitas cargar un archivo csv excel.')
